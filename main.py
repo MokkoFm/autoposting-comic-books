2a118cefc4480315544dcdda8f7c32619579b2c0
MokkoFm <mokkofmpoetry@gmail.com>
2020-09-18 11:16:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import urllib.request
+
+
+def main():
+    url = "https://xkcd.com/info.0.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    comic = response.json()
+    image = urllib.request.urlopen(comic["img"])
+
+    with open("xkcd.png", "wb") as file:
+        content = image.read()
+        file.write(content)
+
+
+if __name__ == "__main__":
+    main()
